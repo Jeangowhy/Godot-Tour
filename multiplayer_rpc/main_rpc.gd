8495c29ce874a19da705f707d7ff042212bf8b43
Jeangowhy <254141203@qq.com>
2023-02-10 07:35:26 +0800

    tune fine

--- multiplayer_rpc/main_rpc.gd
+++ multiplayer_rpc/main_rpc.gd
@@ -19,24 +19,66 @@ var local_player
 func _ready():
 	# Start the server/client by pass "--server" or "--client" argument.
 	if "--server" in OS.get_cmdline_args():
-		# Signals for server
-		multiplayer_peer.peer_disconnected.connect(peer_disconnected)
-		multiplayer_peer.peer_connected.connect(new_peer_connected)
 		start_server()
 	elif "--client" in OS.get_cmdline_args():
-		# Signals for clietns
-		multiplayer.server_disconnected.connect(func(): destroy_peer(get_multiplayer_authority()))
-		multiplayer.connected_to_server.connect(func(): print_debug("Connected to server: %s:%d" % [HOST, PORT]))
-		multiplayer.connection_failed.connect(connection_failed)
 		start_client()
 	else:
 		menu.visible = true
 
-
 	host.pressed.connect(start_server)
 	join.pressed.connect(start_client)
 
+	#message_input.connect("text_submitted", _on_message_input_text_submitted)
+	message_input.connect("focus_entered", _on_message_input_focus_entered)
+	message_input.connect("focus_exited", _on_message_input_focus_exited)
+
+
+func toggle_pause_mode():
+	var mode = get_tree().paused
+	var fps = ProjectSettings.get_setting("physics/common/physics_ticks_per_second", 60)
+	# set_physics_process(false)
+	# set_process(false)
+	get_tree().set_pause(!mode)
+	if mode:
+		Engine.set_physics_ticks_per_second(fps)
+	else:
+		Engine.set_physics_ticks_per_second(fps/3)
+
+
+func _unhandled_input(event):
+	if not event is InputEventKey or not event.is_pressed():
+		return
+	if event.keycode == KEY_ESCAPE:
+		clear_connections()
+	elif event.keycode == KEY_ENTER and not message_input.has_focus():
+		message_input.grab_focus()
+	elif event.keycode == KEY_P:
+		toggle_pause_mode()
+
+func reset_signals(is_server = true):
+	clear_signal(multiplayer_peer.peer_disconnected)
+	clear_signal(multiplayer_peer.peer_connected)
+	clear_signal(multiplayer.server_disconnected)
+	clear_signal(multiplayer.connected_to_server)
+	clear_signal(multiplayer.connection_failed)
+	if is_server:
+		# Signals for server
+		multiplayer_peer.peer_disconnected.connect(peer_disconnected)
+		multiplayer_peer.peer_connected.connect(new_peer_connected)
+	else:
+		# Signals for clietns
+		multiplayer.server_disconnected.connect(func(): destroy_peer(get_multiplayer_authority()))
+		multiplayer.connected_to_server.connect(func(): print_debug("Connected to server: %s:%d" % [HOST, PORT]))
+		multiplayer.connection_failed.connect(connection_failed)
+
+func clear_signal(sgl: Signal):
+	for conn in sgl.get_connections():
+		print_debug("disconnect observer: ", conn["signal"])
+		conn["signal"].disconnect(conn.callable)
+
+
 func start_server():
+	reset_signals(true)
 	message_input.text = "Starting a new server ..."
 	var err = multiplayer_peer.create_server(PORT)
 	if OK != err:
@@ -54,6 +96,7 @@ func start_server():
 
 
 func start_client():
+	reset_signals(false)
 	message_input.text = "Starting a new client ..."
 	var err = multiplayer_peer.create_client(HOST, PORT)
 	if OK != err:
@@ -63,8 +106,9 @@ func start_client():
 	multiplayer.multiplayer_peer = multiplayer_peer
 	menu.visible = false
 	network_side.text = "Client"
-	message_input.text = ""
 	print_debug("Start ENet Client on %s:%d." % [HOST, PORT])
+	await get_tree().create_timer(0.5).timeout
+	message_input.text = ""
 
 
 func connection_failed():
@@ -94,53 +138,70 @@ func create_player(peer_id = 1):
 	if peer_id == multiplayer.get_unique_id():
 		local_player = player
 
+	# exit power saving mode when peers connection comming
+	set_physics_process(true)
+	set_process(true)
+
 
 func peer_disconnected(peer_id):
-	# Destroy player node on other peers
-	rpc("destroy_peer_broadcast", peer_id)
-	# Destroy player node on current peer node.
-	destroy_peer(peer_id)
+	print_debug("peer disconnected: %d." % [peer_id])
+	# Disconnect peer specific from host.
+	# multiplayer_peer.disconnect_peer(peer_id)
+	
+	# Wait ENet for less seconds to keep clean
+	# Destroy player node on other clients peers
+	call_deferred("rpc", "destroy_peer_broadcast", peer_id)
+	#rpc("destroy_peer_broadcast", peer_id)
 
+
+func clear_connections():
+	multiplayer_peer.close()
+	multiplayer.multiplayer_peer = null
+	#get_tree().set_multiplayer_poll_enabled(false)
+	#get_tree().multiplayer_poll = false
+	for id in connected_peer_ids:
+		get_node(str(id)).queue_free()
+	connected_peer_ids.clear()
+	menu.visible = true
+	# power saving when there no connection
+	set_physics_process(false)
+	set_process(false)
 
 func destroy_peer(peer_id):
 	if peer_id == 1:
 		print_debug("Server closed!")
-		multiplayer_peer.close()
-		multiplayer.multiplayer_peer = null
-		#get_tree().set_multiplayer_poll_enabled(false)
-		#get_tree().multiplayer_poll = false
-		clear_players()
-		menu.visible = true
+		clear_connections()
 		return
 	get_node(str(peer_id)).queue_free()
 	connected_peer_ids.remove_at(connected_peer_ids.find(peer_id))
-	rpc("refresh_peer_node_inf")
+	refresh_peer_node_inf()
 
-func clear_players():
-	for id in connected_peer_ids:
-		get_node(str(id)).queue_free()
-	connected_peer_ids.clear()
-
-@rpc("any_peer", "call_local")
-func refresh_peer_node_inf():
-	peer_node_inf.text = "#%d / %d" % [multiplayer.get_unique_id(), len(connected_peer_ids)]
 
 @rpc 
 #@rpc("authority", "call_remote", "reliable", 1)
 func new_peer_broadcast(new_peer_id):
 	create_player(new_peer_id)
 
-@rpc 
-#@rpc("authority", "call_remote", "reliable", 1)
+@rpc("authority", "call_local", "reliable", 1)
 func destroy_peer_broadcast(peer_id):
 	destroy_peer(peer_id)
 
 
-@rpc
-#@rpc("authority", "call_remote", "reliable", 1)
+@rpc("any_peer", "call_local")
+func refresh_peer_node_inf():
+	peer_node_inf.text = "#%d / %d" % [multiplayer.get_unique_id(), len(connected_peer_ids)]
+
+@rpc("any_peer")
 func old_peer_replication(peer_ids):
 	for peer_id in peer_ids:
 		create_player(peer_id)
+
+
+func _on_message_input_focus_entered():
+	local_player.enable_input(false)
+
+func _on_message_input_focus_exited():
+	local_player.enable_input(true)
 
 func _on_message_input_text_submitted(new_text):
 	local_player.rpc("display_message", new_text)
--- multiplayer_rpc/player_rpc.gd
+++ multiplayer_rpc/player_rpc.gd
@@ -1,6 +1,7 @@
 extends MeshInstance3D
 
 var speed = 10
+var can_process_input = true
 
 @onready var message = $Message
 @onready var myname = $Name
@@ -10,7 +11,7 @@ func _ready():
 	myname.text = str(name)
 
 func _physics_process(delta):
-	if multiplayer.has_multiplayer_peer() and is_multiplayer_authority():
+	if can_process_input and multiplayer.has_multiplayer_peer() and is_multiplayer_authority():
 		var axis_x = int(Input.is_key_pressed(KEY_A)) - int(Input.is_key_pressed(KEY_D))
 		var axis_z = int(Input.is_key_pressed(KEY_W)) - int(Input.is_key_pressed(KEY_S))
 		var direction:Vector3 = Vector3(-axis_x, 0, -axis_z)
@@ -18,7 +19,7 @@ func _physics_process(delta):
 		if direction.length():
 			# update my position by myself.
 			global_position += direction.normalized() * delta * speed
-#			# refresh my position of all clients by RPC API.
+			# refresh my position of all clients by RPC API.
 			rpc("remote_set_position", global_position)
 
 func _unhandled_input(event):
@@ -27,6 +28,10 @@ func _unhandled_input(event):
 	match [event.button_index, event.double_click, event.pressed]:
 		[MOUSE_BUTTON_LEFT, true, true]:
 			set_my_message("I'm myself!")
+
+func enable_input(enable):
+	can_process_input = enable
+
 
 func set_my_message(msg):
 	# This api works just for me
@@ -43,30 +48,34 @@ func remote_set_position(pos):
 
 @rpc
 #@rpc("authority", "call_remote", "reliable", 1)
-func display_message(meg):
-	message.text = str(message)
+func display_message(msg):
+	message.text = str(msg)
 
-@rpc("authority", "call_remote", "reliable", 1)
+@rpc
+#@rpc("authority", "call_remote", "reliable", 1)
 func shake_me():
 	message.text = "Shake me!"
 
-@rpc("any_peer", "call_remote", "reliable", 1)
+@rpc("any_peer", "call_local", "reliable", 1)
 func shake_all():
 	message.text = "Shake all!"
 
 @rpc("any_peer", "call_local", "reliable", 1)
 func clicked_by_player():
-	message.text = str(multiplayer.get_remote_sender_id()) + " hit me!"
+	var sender = multiplayer.get_remote_sender_id()
+	message.text = str(sender) + " hit me!"
+	if get_multiplayer_authority() == sender:
+		message.text = "hit myself!"
 
 
-func _on_mouse_click_area_input_event(camera, event, position, normal, shape_idx):
+func _on_area_input_event(_camera, event, _position, _normal, _shape_idx):
 	if not event is InputEventMouseButton:
 		return
 	match event.button_index:
 		MOUSE_BUTTON_LEFT:
 			rpc("clicked_by_player")
 		MOUSE_BUTTON_RIGHT:
-			rpc("shake_me")
+			if is_multiplayer_authority():
+				rpc("shake_me")
 		MOUSE_BUTTON_MIDDLE:
 			rpc("shake_all")
-
